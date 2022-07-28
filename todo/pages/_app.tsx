8563d6a76697d325f2db2639123c138f6937ffa3
matekaszas13 <matekaszas17@gmail.com>
2022-07-28 11:20:56 +0200

    Chore: Add import globals.ccs

--- todo/pages/_app.tsx
+++ todo/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import Nav from '../components/Nav'
 import styles from "../styles/App.module.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
