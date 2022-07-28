5ff29a3da846823f324a0b1f698aace557609000
matekaszas13 <matekaszas17@gmail.com>
2022-07-28 08:54:51 +0200

    Style: Center all content and add margin auto

--- todo/pages/_app.tsx
+++ todo/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app'
 import Nav from '../components/Nav'
+import styles from "../styles/App.module.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={styles.main}>
     <Nav/>
     <Component {...pageProps} />
-    </>
+    </div>
   )
 }
 
