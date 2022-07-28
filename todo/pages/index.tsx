0576eef5a6c892c4b4a4bc5e30d9db8a5861a24d
matekaszas13 <matekaszas17@gmail.com>
2022-07-28 14:21:10 +0200

    Feat: Add Todo component to index.tsx

--- todo/pages/index.tsx
+++ todo/pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from "next";
 import Button from "../components/Button";
+import Todo from "../components/Todo";
 
 const Home: NextPage = () => {
+
+  type Todos = { id: number; name: string };
+
+  const todos: Todos = { id: 1, name: "Take the children from school at 3PM" };
+
   return (
     <div>
       <Button buttonName={"add todo"} />
+      <Todo todo={todos}/>
     </div>
   );
 };
