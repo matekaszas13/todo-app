21570068f3906c0e4ce98df73533aea0364504af
matekaszas13 <matekaszas17@gmail.com>
2022-08-02 12:19:02 +0200

    Feat Add onClick event on trash icon delete todo

--- todo/components/Todo.tsx
+++ todo/components/Todo.tsx
@@ -7,14 +7,15 @@ interface Todo {
   id: number;
   name: string;
   className: string;
+  deleteToDo: Function;
 }
 
-const Todo : React.FC<Todo> = ({id, name, className} : Todo) => {
+const Todo : React.FC<Todo> = ({id, name, className, deleteToDo} : Todo) => {
 
   return (
     <div className={className} id={id.toString()}>
       <span className={styles.todo_text}>{name}</span>
-      <FontAwesomeIcon className={styles.delete_icon} icon={faTrash}/>
+      <FontAwesomeIcon onClick={() => deleteToDo(id)} className={styles.delete_icon} icon={faTrash}/>
     </div>
   );
 };
