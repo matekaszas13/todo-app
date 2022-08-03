7eb8c1c2aa31348b3d12d175f0d4fdd90b379777
matekaszas13 <matekaszas17@gmail.com>
2022-08-03 13:44:51 +0200

    Feat:Add display text based on isCompleted boolean

--- todo/components/Todo.tsx
+++ todo/components/Todo.tsx
@@ -1,11 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 import styles from "../styles/Todo.module.css";
+import { useEffect, useState } from "react";
 
 interface Todo {
   id: number;
   name: string;
-  isFavourite: boolean;
+  isCompleted: boolean;
   className: string;
   deleteToDo: Function;
   changeCheckBoxValue: Function;
@@ -16,13 +17,21 @@ const Todo: React.FC<Todo> = ({
   name,
   className,
   deleteToDo,
-  isFavourite,
+  isCompleted,
   changeCheckBoxValue
 }: Todo) => {
+
+  const [completedText, setCompletedText] = useState<string>("not Completed");
+
+  useEffect(() => {
+    isCompleted ? setCompletedText("Completed") : setCompletedText("not Completed");
+  }, [isCompleted]);
+
   return (
     <div className={className} id={id.toString()}>
       <span className={styles.todo_text}>{name}</span>
-      <input type="checkbox" defaultChecked={isFavourite} onChange={(event) => changeCheckBoxValue(id)}/>
+      <span>{completedText}</span>
+      <input type="checkbox" checked={isCompleted} onChange={() => changeCheckBoxValue(id)}/>
       <FontAwesomeIcon
         onClick={() => deleteToDo(id)}
         className={styles.delete_icon}
