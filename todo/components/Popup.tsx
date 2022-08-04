28a5d8870cf588520621607682c4d68ed67e7b3a
matekaszas13 <matekaszas17@gmail.com>
2022-08-04 21:45:33 +0200

    Feat: Add handle input value and close the modal

--- todo/components/Popup.tsx
+++ todo/components/Popup.tsx
@@ -1,22 +1,27 @@
 import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Button from "./Button";
 import styles from "../styles/Popup.module.css";
+import { useState } from "react";
 
 interface functions {
   setIsOpen: Function;
+  setNewNameForOneTodo: Function;
+  id: number;
 }
 
-const Popup: React.FC<functions> = ({setIsOpen}) => {
+const Popup: React.FC<functions> = ({setIsOpen, setNewNameForOneTodo, id}) => {
+
+  const [inputValue, setInputValue] = useState<string>("");
+
   return (
     <div className={styles.popup_box}>
       <div className={styles.box}>
-        <input type="text" placeholder="Change your plan.." />
-        <Button
-          className={styles.add_btn}
-          buttonName={"add todo"}
-          // addTodo={addTodo}
-        />
+        <input type="text" placeholder="Change your plan.." onChange={(event) => setInputValue(event.target.value)}/>
+        <button className={styles.add_btn}
+          onClick={() => {
+            setNewNameForOneTodo(id, inputValue);
+            setIsOpen(false);
+          }}>add new name</button>
         <FontAwesomeIcon className={styles.close_icon} onClick={() => setIsOpen(false)} icon={faWindowClose} />
       </div>
     </div>
